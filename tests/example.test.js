c703702cf110ecbc60c25eefd086ec5c8b5e16fd
Ellectronx <46204162+Ellectronx@users.noreply.github.com>
2020-05-20 11:34:14 +0200

    Json branch (#3)
    * Move logic to unit.js. Two new tests.
    
    * Two more test...

--- tests/example.test.js
+++ tests/example.test.js
@@ -5,18 +5,19 @@ const T1 = "Mamy 188 nowych i potwierdzonych przypadków zakażenia #koronawirus
 const T2 = "Mamy 301 nowych i potwierdzonych przypadków zakażenia #koronawirus z województw: śląskiego (182), wielkopolskiego (84), dolnośląskiego (10), opolskiego (8), podlaskiego (6), pomorskiego (4), świętokrzyskiego (2), małopolskiego (2), lubelskiego (2) i zachodniopomorskiego (1).";
 
 describe('Simple Math Test', () => {
-    it('should return 2', () => {
-            assert.equal(1 + 1, 2);
-        });
-    it('should return 9', () => {
-            assert.equal(3 * 3, 9);
-        });
-
     it('should return 16 - woj. dolnośląskie:T1', () => {
             assert.equal(  UTIL.getJSON(T1)['0'] , 16);
         });
 
     it('should return 2 - woj. lubelskie:T2', () => {
         assert.equal(  UTIL.getJSON(T2)['2'] , 2);
+    });
+
+    it('should return 113 - woj. śląskie:T1', () => {
+        assert.equal(  UTIL.getJSON(T1)['11'] , 113);
+    });
+
+    it('should return 1 - woj. zachodniopomorskie:T1', () => {
+        assert.equal(  UTIL.getJSON(T2)['15'] , 1);
     });
 });
